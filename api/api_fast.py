b7ce35891247cb9250a5cb9c9c33e33f8c5545d9
Sean Diacono <seandiacono@gmail.com>
2021-05-04 12:07:07 +0200

    :sparkles: Merged all systems into API

--- api/api_fast.py
+++ api/api_fast.py
@@ -13,6 +13,7 @@ from torchvision import transforms as T
 import segmentation_models_pytorch as smp
 from PIL import Image
 from scipy import stats
+from MonoDepth2.depth_predicter import DepthFinder
 import threading
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -36,6 +37,45 @@ class DroneController(BaseModel):
     status: Optional[Any] = "Idle"
     time: Optional[float] = 0.0
     encoded_img: Optional[Any] = ""
+
+    def evasive_manouver(depth_img):
+        global client
+
+        height, width = depth_img.shape
+
+        left = depth_img[0:height, 0:(width // 2)].copy()
+        right = depth_img[0:height, (width // 2):width].copy()
+
+        left_avg = np.average(left)
+        right_avg = np.average(right)
+
+        if left_avg > right_avg:
+            print("GOING RIGHT")
+            client.moveByVelocityBodyFrameAsync(0, 1, 0, 1).join()
+            client.moveByVelocityAsync(0, 0, 0, 1).join()
+        else:
+            print("GOING LEFT")
+            client.moveByVelocityBodyFrameAsync(0, -1, 0, 1).join()
+            client.moveByVelocityAsync(0, 0, 0, 1).join()
+
+    def percent_landing_zone(img):
+        unique, counts = np.unique(
+            img.reshape(-1, img.shape[2]), axis=0, return_counts=True)
+
+        tuples = [tuple(x) for x in unique]
+
+        total = sum(counts)
+
+        percentages = [(x / total) * 100 for x in counts]
+
+        dict_percentages = dict(zip(tuples, percentages))
+
+        try:
+            landing_zone_percent = dict_percentages[(128, 64, 128)]
+        except:
+            landing_zone_percent = 0.0
+
+        return landing_zone_percent
 
     def predict_image(self, model, image, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
         model.eval()
@@ -91,6 +131,7 @@ class DroneController(BaseModel):
 
         newX = self.xCoord
         newY = self.yCoord
+
         while not landed:
             img = airsim.string_to_uint8_array(
                 client.simGetImage("bottom_center", airsim.ImageType.Scene))
@@ -107,8 +148,8 @@ class DroneController(BaseModel):
 
             height, width, _ = img.shape
 
-            new_height = 64
-            new_width = 64
+            new_height = 100
+            new_width = 100
 
             upper_left = (int((width - new_width) // 2),
                           int((height - new_height) // 2))
@@ -116,26 +157,53 @@ class DroneController(BaseModel):
                             int((height + new_height) // 2))
 
             img = pred_mask[upper_left[1]: bottom_right[1],
-                            upper_left[0]: bottom_right[0]]
+                            upper_left[0]: bottom_right[0]].copy()
 
-            img = np.asarray([img])
+            landing_zone_percent = self.percent_landing_zone(img)
 
-            mode = self.bincount_app(img)
-
-            if mode == (128, 64, 128):
-                cv2.imwrite("../land_mask.png", pred_mask)
-                client.moveByVelocityAsync(0, 0, 0, 2).join()
+            if landing_zone_percent > 95.0:
                 client.moveToPositionAsync(
-                    self.xCoord, self.yCoord, -5, 1).join()
-                client.landAsync().join()
-                self.status = "Landed"
+                    newX, newY, 1, 2).join()
+                client.landAsync()
                 landed = True
-                return
             else:
-                newX += 0.5
-                newY += 0.5
+                height, width, _ = pred_mask.shape
+
+                bottom_left = pred_mask[(height // 2)
+                                         :height, 0:(width // 2)].copy()
+                bottom_right = pred_mask[(height // 2):height,
+                                         (width // 2):width].copy()
+                top_left = pred_mask[0:(height // 2), 0:(width // 2)].copy()
+                top_right = pred_mask[0:(height // 2),
+                                      (width // 2):width].copy()
+
+                top_left_percent = self.percent_landing_zone(top_left)
+                top_right_percent = self.percent_landing_zone(top_right)
+                bottom_left_percent = self.percent_landing_zone(bottom_left)
+                bottom_right_percent = self.percent_landing_zone(bottom_right)
+
+                if top_left_percent > max(top_right_percent, bottom_left_percent, bottom_right_percent):
+                    print("Moving Top left")
+                    newX += 2
+                    newY += -2
+                elif top_right_percent > max(top_left_percent, bottom_left_percent, bottom_right_percent):
+                    print("Moving Top right")
+                    newX += 2
+                    newY += 2
+                elif bottom_left_percent > max(top_left_percent, top_right_percent, bottom_right_percent):
+                    print("Moving bottom left")
+                    newX += -2
+                    newY += -2
+                elif bottom_right_percent > max(top_left_percent, top_right_percent, bottom_left_percent):
+                    print("Moving bottom right")
+                    newX += -2
+                    newY += 2
+
                 client.moveToPositionAsync(
-                    newX, newY, self.altitude, 0.5).join()
+                    newX, newY, -30, 1).join()
+                client.moveByVelocityAsync(0, 0, 0, 1).join()
+
+        return
 
     def get_status(self):
         return {"progress": self.progress, "time_left": int(self.time), "status": self.status, "image": self.encoded_img}
@@ -173,7 +241,8 @@ class DroneController(BaseModel):
             current_dist = distance.euclidean([position.x_val, position.y_val, position.z_val], [
                 self.xCoord, self.yCoord, self.altitude])
 
-            self.progress = int(100-((current_dist/self.dist_to_dest) * 100))
+            self.progress = int(
+                100 - ((current_dist / self.dist_to_dest) * 100))
 
             self.time = current_dist / self.velocity
 
@@ -183,6 +252,40 @@ class DroneController(BaseModel):
             decoded_frame = cv2.imdecode(np_response_image, cv2.IMREAD_COLOR)
             _, self.encoded_img = cv2.imencode(DECODE_EXTENSION, decoded_frame)
             self.encoded_img = base64.b64encode(self.encoded_img)
+
+            depth = depth_finder.get_depth_map(decoded_frame)
+            normalizedImg = cv2.normalize(
+                depth, None, 0, 255, cv2.NORM_MINMAX)
+
+            ret, thresh = cv2.threshold(
+                normalizedImg, 175, np.amax(normalizedImg), cv2.THRESH_BINARY)
+
+            height, width, _ = decoded_frame.shape
+
+            new_height = 200
+            new_width = 200
+
+            upper_left = (int((width - new_width) // 2),
+                          int((height - new_height) // 2))
+            bottom_right = (int((width + new_width) // 2),
+                            int((height + new_height) // 2))
+
+            crop_img = thresh[upper_left[1]: bottom_right[1],
+                              upper_left[0]: bottom_right[0]].copy()
+
+            average_depth = np.average(crop_img)
+
+            # print(average_depth)
+
+            if average_depth > 20:
+                # print("TOO CLOSE TO OBJECT - STOPPING AND HOVERING")
+                client.moveByVelocityAsync(0, 0, 0, 1).join()
+                client.hoverAsync().join()
+                # print("TAKING EVASIVE MANOUVER")
+                self.evasive_manouver(crop_img)
+                # print("done")
+                client.moveToPositionAsync(
+                    self.xCoord, self.yCoord, self.altitude, self.velocity)
 
             if (self.progress > 95):
                 client.moveByVelocityAsync(0, 0, 0, 1).join()
@@ -212,7 +315,8 @@ class DroneController(BaseModel):
             current_dist = distance.euclidean([position.x_val, position.y_val, position.z_val], [
                 0, 0, self.altitude])
 
-            self.progress = int(100-((current_dist/self.dist_to_dest) * 100))
+            self.progress = int(
+                100 - ((current_dist / self.dist_to_dest) * 100))
 
             self.time = current_dist / self.velocity
 
@@ -247,6 +351,8 @@ drone_controller = DroneController()
 # model = torch.load('../models/Unet-Mobilenet.pt')
 
 model = torch.load('../models/DeepLabV3Plus-Mobilenet.pt')
+
+depth_finder = DepthFinder("mono+stereo_640x192")
 
 landing_thread = threading.Thread(target=drone_controller.landing)
 
@@ -296,24 +402,19 @@ async def set_flight_params(drone_controller_temp: DroneController):
     return status
 
 
-@app.get('/videoStream')
-async def get_video_stream():
-    return StreamingResponse(frame_generator(), media_type="multipart/x-mixed-replace;boundary=frame")
+# @app.get('/bottom_cam_image')
+# async def get_bottom_image():
+#     CAMERA_NAME = '0'
+#     IMAGE_TYPE = airsim.ImageType.Scene
+#     DECODE_EXTENSION = '.png'
 
-
-@app.get('/bottom_cam_image')
-async def get_bottom_image():
-    CAMERA_NAME = '0'
-    IMAGE_TYPE = airsim.ImageType.Scene
-    DECODE_EXTENSION = '.png'
-
-    response_image = client.simGetImage(CAMERA_NAME, IMAGE_TYPE)
-    np_response_image = np.asarray(
-        bytearray(response_image), dtype="uint8")
-    decoded_frame = cv2.imdecode(np_response_image, cv2.IMREAD_COLOR)
-    _, encoded_img = cv2.imencode(DECODE_EXTENSION, decoded_frame)
-    encoded_img = base64.b64encode(encoded_img)
-    return {"img": encoded_img}
+#     response_image = client.simGetImage(CAMERA_NAME, IMAGE_TYPE)
+#     np_response_image = np.asarray(
+#         bytearray(response_image), dtype="uint8")
+#     decoded_frame = cv2.imdecode(np_response_image, cv2.IMREAD_COLOR)
+#     _, encoded_img = cv2.imencode(DECODE_EXTENSION, decoded_frame)
+#     encoded_img = base64.b64encode(encoded_img)
+#     return {"img": encoded_img}
 
 
 @app.get('/get_trip_details')
